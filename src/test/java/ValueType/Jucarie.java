9a14bce508a8ec278c1e7cd8ba5897d678ba76f8
Irinamarciuc28 <marciuc.irina@yahoo.com>
2023-01-26 19:06:23 +0200

    am invatat despre value type

---
+++ src/test/java/ValueType/Jucarie.java
@@ -0,0 +1,26 @@
+package ValueType;
+
+public class Jucarie {
+
+    public String Nume;
+
+    public Jucarie(String nume) {
+        Nume = nume;
+    }
+
+    public String getNume() {
+        return Nume;
+    }
+
+    public void setNume(String nume) {
+        Nume = nume;
+    }
+
+    public void Nume(){
+        Nume.Info();
+
+    }
+
+    public void Info() {
+    }
+}
